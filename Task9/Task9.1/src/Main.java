9fc941e1a4ee155ab2955fd6cf99701110672206
Igorexa <Igoryan818@yandex.ru>
2022-10-04 00:05:45 +0300

    Task 9.1

---
+++ Task9/Task9.1/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        String text = "Это просто текст, для примера работы программы";
+        //(1) Это (2) просто (3) текст, (4) для (5) примера (6) работы (7) программы
+        StringBuilder resText = new StringBuilder();
+        int index = 0;
+        int count = 1;
+        int space;
+
+        while(index >= 0) {
+            space = text.indexOf(" ", index + 1);
+            resText.append("(").append(count).append(") ");
+            if(space == -1) {
+                resText.append(text.substring(index));
+                break;
+            } else {
+                resText.append(text, index, space + 1);
+            }
+            index = space + 1;
+            count++;
+        }
+
+        System.out.println(resText);
+
+
+
+    }
+}
