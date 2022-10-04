fa22621f95bfb1c0ab44896780932ee75892f1d8
Igorexa <Igoryan818@yandex.ru>
2022-10-04 21:15:53 +0300

    fix task 9.1

--- Task9/Task9.1/src/Main.java
+++ Task9/Task9.1/src/Main.java
@@ -1,18 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     /**
      * @param args
-     * из Это просто текст, для примера работы программы преобразовать в
-     * (1) Это (2) просто (3) текст, (4) для (5) примера (6) работы (7) программы
+     * из "Это просто текст, для примера работы программы" преобразовать в
+     * "(1) Это (2) просто (3) текст, (4) для (5) примера (6) работы (7) программы"
      */
     public static void main(String[] args) {
-        String text = "Это просто текст, для примера работы программы";
-        //(1) Это (2) просто (3) текст, (4) для (5) примера (6) работы (7) программы
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        //String text = "Это просто текст, для примера работы программы";
+        System.out.println(sequentialWordsNumbers(text));
+        //System.out.println(text.lastIndexOf(""));
+
+    }
+
+    public static StringBuilder sequentialWordsNumbers(String text) {
+
         StringBuilder resText = new StringBuilder();
         int index = 0;
         int count = 1;
         int space;
 
-        while(index >= 0) {
+        while(index <= text.lastIndexOf("")) {
             space = text.indexOf(" ", index + 1);
             resText.append("(").append(count).append(") ");
             if(space == -1) {
@@ -25,9 +35,8 @@ public class Main {
             count++;
         }
 
-        System.out.println(resText);
 
-
+        return resText;
 
     }
 }
