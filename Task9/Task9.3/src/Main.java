d065b0a8d4cdc53558d6a8cde2d39509e30b3384
Igorexa <Igoryan818@yandex.ru>
2022-10-06 22:41:47 +0300

    Task 9.3

--- Task9/Task9.3/src/Main.java
+++ Task9/Task9.3/src/Main.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        while(true){
+            String phoneNumber = new Scanner(System.in).nextLine();
+            System.out.println(formatPhoneNumber(phoneNumber));
+        }
+
+
+
+
+
+    }
+
+    public static String formatPhoneNumber(String phone) {
+        String clearRegex = "[^0-9]";
+        String firstNumber = "[8][0-9]{10}";
+        String regex = "[7][0-9]{10}";
+
+        phone = phone.replaceAll(clearRegex, "");
+        if (phone.matches(firstNumber)) {
+            phone = phone.replace("8", "7");
+        } if(phone.matches(regex)){
+            return phone;
+        } else return "Неверный формат номера";
+    }
 }
