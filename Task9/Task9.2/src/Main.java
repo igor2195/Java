4a4b2424d5c744b1e9fc203fce8705adf84eb1ec
Igorexa <Igoryan818@yandex.ru>
2022-10-06 21:41:42 +0300

    Task 9.2

--- Task9/Task9.2/src/Main.java
+++ Task9/Task9.2/src/Main.java
@@ -3,12 +3,58 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String first = new Scanner(System.in).nextLine();
-        System.out.println(first);
+        String inputString = new Scanner(System.in).nextLine();
+        System.out.println(SplitText(inputString));
+
 
     }
 
-    public static String SplitText (String text) {
-        return "";
+    /**
+     * @param inputString исходная строка
+     * @return возвращает слова из исходной строки разбитые через \n по пробелу
+     */
+    public static String SplitText(String inputString) {
+        int currentIndex = 0;
+        int lastStringIndex = inputString.lastIndexOf("");
+        int spaceIndex;
+        String newLine = System.lineSeparator();
+        StringBuilder outString = new StringBuilder();
+
+
+        while (currentIndex <= lastStringIndex) {
+            spaceIndex = inputString.indexOf(" ", currentIndex);
+
+            if (spaceIndex == -1) {
+                outString.append(inputString.substring(currentIndex));
+                break;
+            } else {
+                outString.append(inputString, currentIndex, spaceIndex).append(newLine);
+            }
+            currentIndex = spaceIndex + 1;
+
+        }
+        return outString.toString();
     }
+
+
+
+
+        /*while(index <= text.lastIndexOf("")) {
+            space = text.indexOf(" ", index + 1);
+            resText.append("(").append(count).append(") ");
+            if(space == -1) {
+                resText.append(text.substring(index));
+                break;
+            } else {
+                resText.append(text, index, space + 1);
+            }
+            index = space + 1;
+            count++;
+        }
+
+
+        return resText.toString();
+        */
+
+
 }
