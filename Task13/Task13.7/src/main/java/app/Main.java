58e10997a2b971cb8a084e101916e872eeab70dc
Igorexa <Igoryan818@yandex.ru>
2022-12-06 01:32:55 +0300

    Task 13.7

---
+++ Task13/Task13.7/src/main/java/app/Main.java
@@ -0,0 +1,23 @@
+package app;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+public class Main {
+
+    private static final String STAFF_TXT = "data/staff.txt";
+
+    public static void main(String[] args) {
+        List<Employee> staff = EmployeeUtils.loadStaffFromFile(STAFF_TXT);
+        Employee employeeMaxSalary = findEmployeeWithHighestSalary(staff, 2017);
+        System.out.println(employeeMaxSalary);
+    }
+
+    public static Employee findEmployeeWithHighestSalary(List<Employee> staff, int year) {
+        //TODO Метод должен вернуть сотрудника с максимальной зарплатой среди тех,
+        // кто пришёл в году, указанном в переменной year
+        Optional<Employee> res = staff.stream().filter(e -> e.getWorkStart().getYear() == year).max(Comparator.comparing(Employee::getSalary));
+        return res.get();
+    }
+}
